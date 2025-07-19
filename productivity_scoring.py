6777fd0ee5926993c1f9fe9e92d5465fa2cac207
MacCready <paulcharbel@proton.me>
2025-07-19 15:38:47 +0200

    added comments to productivity_scoring.py

--- productivity_scoring.py
+++ productivity_scoring.py
@@ -2,12 +2,17 @@ import json
 import re
 
 class ScoringEngine:
+    """A class made to process the data received from the AW API
+       and calculate the score of an activity depending on the multiplier
+       in config.json """
+
     def __init__(self, config_path='config.json'):
         self.categories = {}
 
         with open(config_path, 'r', encoding='utf-8') as f:
             data = json.load(f)
 
+        #We fetch every rule in the config.json and assign it to a class variable
         for item in data["categories"]:
             name = item["name"]
             self.categories[name] = {
@@ -15,6 +20,12 @@ class ScoringEngine:
                 "multiplier": item.get("multiplier", 1),
                 "parent": item.get("parent", None)
             }
+
+    #####The scoring logic#####
+    # Here, we go through every event passed to the method
+    # to get it's category and then give it a score depending 
+    # on it's multiplier and the duration of that event
+
 
     def score(self, input_text: str, base_value: float) -> float:
         for cat, data in self.categories.items():
